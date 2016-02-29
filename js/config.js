423ff8627925b1e67993c843e42a2db8be073cc0
yangli <yang.li@rea-group.com>
2016-02-29 23:07:49 +0800

    fixed test issue -Yang

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-base_url = "http://localhost:80/marketing_promotion/";
+base_url = "http://localhost:63342/marketing_promotion/";
 api_base_url = "http://localhost:8080/";
-image_base_url = "http://localhost:80/";
+image_base_url = "http://localhost:63342/";
