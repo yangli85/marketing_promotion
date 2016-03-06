83d313ac8960d7750d974634931c74c4ac8b0b2c
yangli <yang.li@rea-group.com>
2016-03-06 15:27:46 +0800

    fixed test issue -Yang

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 base_url = "http://localhost:63342/marketing_promotion/";
 api_base_url = "http://localhost:8080/";
-image_base_url = "http://localhost:63342/";
+image_base_url = "http://localhost:8080/athena-api/";
