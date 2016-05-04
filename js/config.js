091d3485cb6ca1f99f130cc23038e8ea38f63768
yangli85 <yangli@CNyangli.local>
2016-05-04 14:13:00 +0800

    change config -yang

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 //base_url = "http://localhost:63342/marketing_promotion/";
 //api_base_url = "http://localhost:8080/";
 //image_base_url = "http://localhost:8080/athena-api/";
-base_url = "http://101.200.162.121/marketing_promotion/";
-api_base_url = "http://101.200.162.121:8080/";
-image_base_url = "http://101.200.162.121/athena-api/";
+base_url = "http://101.201.79.44/marketing_promotion/";
+api_base_url = "http://101.201.79.44:8080/";
+image_base_url = "http://101.201.79.44";
