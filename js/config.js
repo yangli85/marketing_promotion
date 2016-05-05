69f1284f1653a6f55ac6cdac7a365adf9badd8b3
yangli85 <yangli@CNyangli.local>
2016-05-05 15:50:00 +0800

    refactor wechat share page -Yang

--- js/config.js
+++ js/config.js
@@ -3,4 +3,4 @@
 //image_base_url = "http://localhost:8080/athena-api/";
 base_url = "http://101.201.79.44/marketing_promotion/";
 api_base_url = "http://101.201.79.44:8080/";
-image_base_url = "http://101.201.79.44";
+image_base_url = "http://101.201.79.44/";
